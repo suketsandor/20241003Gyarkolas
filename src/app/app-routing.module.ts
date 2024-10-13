323ef794b730f0f74a5d416561343d26c3606541
suketsandor <ifj.suket.sandor@gmail.com>
2024-10-13 10:39:03 +0200

    10.03 feladat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooldalComponent } from './fooldal/fooldal.component';
+import { RolunkComponent } from './rolunk/rolunk.component';
+import { ButorokComponent } from './butorok/butorok.component';
+import { errorContext } from 'rxjs/internal/util/errorContext';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"fooldal", component:FooldalComponent},
+  {path:"rolunk", component:RolunkComponent},
+  {path:"butorok", component:ButorokComponent},
+
+  {path:"", redirectTo:"/fooldal", pathMatch:"full"},
+
+  {path:"**", component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FooldalComponent } from './fooldal/fooldal.component';
+import { RolunkComponent } from './rolunk/rolunk.component';
+import { ButorokComponent } from './butorok/butorok.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooldalComponent,
+    RolunkComponent,
+    ButorokComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
