a65539294e71a383db5f0520b82f49af3d09f226
suketsandor <ifj.suket.sandor@gmail.com>
2024-10-13 13:58:55 +0200

    butorok-form-tombFelvitel feladat

--- src/app/butorok/butorok.component.ts
+++ src/app/butorok/butorok.component.ts
@@ -1,3 +1,4 @@
+import { NumberSymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './butorok.component.css'
 })
 export class ButorokComponent {
+ tomb:Butor[] = [];
+ cikkszam:string = "";
+ megnevezes:string = "";
+ ar:number = 0;
+ db:number = 0;
 
+ felvitel():void {
+  var aktButor = {
+    cikkszam: this.cikkszam,
+    megnevezes: this.megnevezes,
+    ar: this.ar,
+    db: this.db
+  }
+
+  this.tomb.push(aktButor);
+
+  this.cikkszam = "";
+  this.megnevezes = "";
+  this.ar = 0
+  this.db = 0
+
+ }
+}
+
+export interface Butor{
+  cikkszam:string,
+  megnevezes:string,
+  ar:number,
+  db:number
 }
